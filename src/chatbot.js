4b17d947b65081cd82db2b4900c6a3e92a0c385c
Hazem Shaban <hazemshaban@192.168.1.10>
2025-11-09 21:58:02 +0200

    addding voice chatbot

--- src/chatbot.js
+++ src/chatbot.js
@@ -539,7 +539,7 @@ this.createLoadingSound = () => {
       }
       let { room, token } = await this.getVoiceToken("user1", "Hazem");
       
-      await this.voiceCallState.room.connect('wss://finovax.duckdns.org:7881', token);
+      await this.voiceCallState.room.connect('wss://finovax.duckdns.org', token);
       console.log('Connected to room', this.voiceCallState.room.name);
       
       await fetch(endpoints.chatbot.start_agent, {
